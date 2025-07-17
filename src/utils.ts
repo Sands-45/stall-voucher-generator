4e11aa7fe0b0dfa08451b01d8e7763793189f0bd
Sands-45 <sandsqa@hotmail.com>
2025-07-18 01:14:36 +0200

    Minor update

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { v4 as uuid } from "uuid";
 
 const generateVoucher = (percentage: number): string => {
+  console.log("Generating a discount or voucher");
   const mockRandom = uuid() + percentage;
 
   return mockRandom;
