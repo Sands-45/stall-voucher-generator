3f0f5fada214438ac29f5f4b74824cda501c7270
Sands-45 <sandsqa@hotmail.com>
2025-07-19 05:01:25 +0200

    Updating to the new arch

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import type { StallExtension } from "@stallpos/extension-types";
+
+const app: StallExtension = {
+  pages: [],
+};
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Don't make any changes to this file
 export * as utils from "./utils";
-export { default as app } from "./app.json";
+export { default as app } from "./app";
 export { default as config } from "./extension.toml";
+
